8f9fd695546e4748e2fe61e859f017238d9eee1e
mirko25 <mirkodelmoro25@gmail.com>
2019-04-01 21:01:04 +0200

    aggiunto metodo per modificare valore

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@
 using namespace std;
 
 int main() {
+    FileIni file("FirstProgram.ini");
+    cout << "File Name: "<< file.getFileName() << endl;
+
+    file.setBoolValue("Sezione", "Parametro", true);
+    file.setFloatValue("Sezione", "Parametro", 10.34);
+    file.setIntValue("Sezione", "Parametro", 4);
+    file.setStringValue("Sezione", "Parametro", "Valore");
+
+    file.checkIsOpen();
+
+
 
     
     
